1ba26b2a501c29546b0d6714e49c5b23a04faea7
laitathei <56575156+laitathei@users.noreply.github.com>
2021-09-12 01:11:07 +0800

    Update attention_pose_set.py

--- script/attention_pose_set.py
+++ script/attention_pose_set.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import rospy
-import math
 from geometry_msgs.msg import PoseStamped
 from jsk_recognition_msgs.msg import BoundingBox, BoundingBoxArray
 rospy.init_node("attention_pose_set")
@@ -11,14 +10,14 @@ r = rospy.Rate(10)
 theta = 0
 while not rospy.is_shutdown():
     boxes = BoundingBoxArray()
-    theta = math.fmod(theta + 0.1, math.pi * 2)
     box = BoundingBox()
     box.header.stamp = rospy.Time.now()
     box.header.frame_id = "d435_depth_optical_frame"
     box.pose.orientation.w = 1
-    box.pose.position.x = 0 # increase in program  = move to right in rviz
-    box.pose.position.y = -0.2 # increase in program  = downward in rviz
-    box.pose.position.z = 0.8 # increase in program  = move away in rviz
+
+    box.pose.position.x = 0.0265447860956 # increase in program  = move to right in rviz (directly use center_world_x)
+    box.pose.position.y = 0.0913378709553 # increase in program  = downward in rviz (directly use center_world_y)
+    box.pose.position.z = 1.8839999437332153 # increase in program  = move away in rviz (directly use the depth distance)
     box.dimensions.x = 0.5
     box.dimensions.y = 0.5
     box.dimensions.z = 0.5
